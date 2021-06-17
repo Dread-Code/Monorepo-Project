a91946dc07a56e23d3a547d6bc86084c27e45785
Lucas Ospina A <lucas.ospina@grupokonecta.com>
2021-06-17 15:44:16 -0500

    babel loader configured

---
+++ packages/usp-components/index.js
@@ -0,0 +1,1 @@
+export const suma = () => console.log("suma")
--- packages/usp-components/webpack.config.js
+++ packages/usp-components/webpack.config.js
@@ -11,5 +11,16 @@ module.exports = {
     },
     resolve: {
         extensions:['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
     }
 }
