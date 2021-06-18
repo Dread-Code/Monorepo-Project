15a1423086d7e52c205b9fd09c50ce0d92c958a6
DreadfulCrazy <lucasospina97@gmail.com>
2021-06-17 23:12:54 -0500

    css-minimizer-webpack-plugin removed

--- packages/usp-components/webpack.config.js
+++ packages/usp-components/webpack.config.js
@@ -1,48 +1,44 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
-const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: { import: './index.js'},
-    mode: 'production',
-    devtool: 'source-map',
-    output: {
-        filename: 'componentes.bundle.js',
-        path: path.resolve(__dirname, 'build'),
-        library: 'usp-componentes',
-        libraryTarget: 'umd'
-    },
-    resolve: {
-        extensions:['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.(s[as]ss|css)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    plugins:[
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        }),
-        new CleanWebpackPlugin()
+  entry: { import: "./index.js" },
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    filename: "componentes.bundle.js",
+    path: path.resolve(__dirname, "build"),
+    library: "usp-componentes",
+    libraryTarget: "umd",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(s[as]ss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
-    optimization:{
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
-        ]
-    }
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
