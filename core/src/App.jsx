387b1bb8c5e79af156a9297e7c496dfc703b7b72
DreadfulCrazy <lucasospina97@gmail.com>
2021-06-18 14:37:41 -0500

    Global eslint configured

--- core/src/App.jsx
+++ core/src/App.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { Button } from '@usp/components'
 import { hot } from 'react-hot-loader/root'
 
-const App = () => {
-  return (
-    <div>
-      <h1>App...</h1>
-      <Button primary="true" label="Button" />
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <h1>App...</h1>
+    <Button primary="true" label="Button" />
+  </div>
+)
 
 export default hot(App)
